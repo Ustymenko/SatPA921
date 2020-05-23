192d08e2f1455123d55cf00c4eef46b871ed7081
Ustymenko <ustymyar@ukr.net>
2020-05-23 21:22:38 +0300

    23052020

---
+++ C#/16052020/16052020/16052020-1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16052020
+{
+    static class MyExtension
+    {
+        static public void Print<T>(this List<T> list, char ch = '\t')
+        {
+            foreach (var item in list)
+                Console.Write($"{item}" + ch);
+            Console.Write("\n--------------------------------------------\n");
+        }
+        static public void Print<T>(this LinkedList<T> list, char ch = '\t')
+        {
+            foreach (var item in list)
+                Console.Write($"{item}" + ch);
+            Console.Write("\n--------------------------------------------\n");
+        }
+        static public void Print<Tk, Tv>(this Dictionary<Tk,Tv> dic, char ch = '\t')
+        {
+            foreach (var item in dic)
+                Console.Write($"{item.Key,-10} {item.Value}" + ch);
+            Console.Write("\n--------------------------------------------\n");
+        }
+
+    }
+    class CMPdate : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            if (x is null || y is null) return 1;
+            return x.Bday.CompareTo(y.Bday);
+        }
+    }
+    class Student : IComparable<Student>, IEquatable<Student>
+    {
+        public string Name { get; set; }
+        public DateTime Bday { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            //  Name.CompareTo(other.Name)
+            return String.Compare(Name, other.Name);
+        }
+
+        public override string ToString()
+        {
+            return $" {Name,-10} {Bday.ToShortDateString()}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.ToString() == obj.ToString();
+        }
+
+        public bool Equals(Student other)
+        {
+            return other != null &&
+                   Name == other.Name &&
+                   Bday == other.Bday;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1892292941;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + Bday.GetHashCode();
+            return hashCode;
+        }
+    }
+
+
+
+    class Program
+    {
+        static public int CompareDate(Student x, Student y)
+        {
+            if (x is null || y is null) return 1;
+            return x.Bday.CompareTo(y.Bday);
+        }
+
+        static void TestList()
+        {
+
+            List<int> list1 = new List<int>();
+            list1.Add(10);
+            list1.AddRange(new int[] { 20, 3, 40, 5, 60, 7 });
+            list1.Sort();
+            // list1.Reverse();
+            list1.Print();
+
+
+            // list1.Add(10.96);
+            List<double> list2 = new List<double>();
+            list2.AddRange(new double[] { 2.330, 3, 4.0, 5.69, 60, 7 });
+            list2.Sort();
+            list2.Print();
+
+
+            List<Student> gr = new List<Student>();
+            gr.Add(new Student { Name = "Ivan", Bday = new DateTime(1998, 10, 23) });
+            gr.Add(new Student { Name = "Anna", Bday = new DateTime(2010, 10, 23) });
+            gr.Add(null);
+            gr.Add(new Student { Name = "Piter", Bday = new DateTime(2013, 10, 23) });
+            gr.Add(new Student { Name = "Igor", Bday = new DateTime(2000, 10, 23) });
+            gr.Print('\n');
+            gr.Sort();
+            gr.Print('\n');
+            //gr.Sort(new CMPdate());
+            //   gr.Sort(CompareDate);
+            gr.Sort((a, b) => (a is null || b is null) ? 1 : a.Bday.CompareTo(b.Bday));
+            gr.Print('\n');
+
+            gr.RemoveAt(0);
+            gr.Print('\n');
+
+
+        }
+
+        static void TestLinkedList()
+        {
+            LinkedList<Student> students = new LinkedList<Student>();
+
+            LinkedListNode<Student> nodeIvan = students.AddFirst(
+                new Student { Name = "Ivan", Bday = new DateTime(1998, 10, 23) });
+
+            students.AddLast(
+                new Student { Name = "Piter", Bday = new DateTime(2013, 10, 23) });
+
+            students.AddAfter(nodeIvan, new Student { Name = "Anna", Bday = new DateTime(2010, 10, 23) });
+            students.AddBefore(nodeIvan, new Student { Name = "Ira", Bday = new DateTime(2014, 10, 23) });
+
+            //students[1] error O(1)
+            students.Print('\n');
+
+            Console.WriteLine(nodeIvan.Next.Next.Value);
+            Console.WriteLine(nodeIvan.Previous.Value);
+
+            var node = students.Find(new Student { Name = "Piter", Bday = new DateTime(2013, 10, 23) });
+
+            if (node is null)
+                Console.WriteLine("no");
+            else
+                Console.WriteLine("yes");
+            // students.sort -error
+
+            // var sss= students.OrderBy(s => s).ToList();
+            //          ///sss.Print('\n');
+            //students = new LinkedList<Student>(students.OrderBy(s => s));
+            //students.Print('\n');
+
+            //students = new LinkedList<Student>(students.OrderByDescending(s => s));
+            //students.Print('\n');
+
+            students = new LinkedList<Student>(students.OrderBy(s => s.Bday));
+            students.Print('\n');
+
+
+        }
+
+
+        static void TestDictionary()
+        {
+            Dictionary<string, Student> dic = new Dictionary<string, Student>();
+
+            dic.Add("Ivan",new Student { Name = "Ivan", Bday = new DateTime(1998, 10, 23) });
+            dic.Add("Anna", new Student { Name = "Anna", Bday = new DateTime(2010, 10, 23) });
+            dic.Add("Piter", new Student { Name = "Piter", Bday = new DateTime(2013, 10, 23) });
+            dic.Add("Igor", new Student { Name = "Igor", Bday = new DateTime(2000, 10, 23) });
+          //  dic.Add("Igor", new Student { Name = "Igor", Bday = new DateTime(2000, 10, 23) });
+
+            dic["Stepan"] = new Student { Name = "Stepan", Bday = new DateTime(1963, 10, 23) };
+
+            dic.Print('\n');
+
+            if (dic.ContainsKey("Igor"))
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+
+            if (dic.ContainsValue(new Student { Name = "Stepan", Bday = new DateTime(1963, 10, 23) }))
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+            Console.WriteLine(dic.Count);
+            dic.Remove("Igor");
+            dic.Print('\n');
+           
+
+        }
+        static void Main(string[] args)
+        {
+            //TestList();
+            //TestLinkedList();
+            TestDictionary();
+        }
+    }
+}
---
+++ C#/23052020/23052020/23052020.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23052020
+{
+    //event
+    class Student
+    {
+        public String Name { get; set; }
+        public DateTime Bday { get; set; }
+        public List<int> marks = new List<int>();//5,3,10,11,8
+        public void SetMark(int m) => marks.Add(m);
+        public override string ToString() => $"|{Name,-10}|{Bday.ToShortDateString(),12}|" +
+                $"{String.Join(", ", marks)}|";
+    }
+    class Aspirant
+    {
+        public String Name { get; set; }
+        public DateTime Bday { get; set; }
+        public List<int> marks = new List<int>();//5,3,10,11,8
+        public void SetOcinka(int m) => marks.Add(m);
+        public override string ToString() => $"|{Name,-10}|{Bday.ToShortDateString(),12}|" +
+                $"{String.Join(", ", marks)}|";
+    }
+    delegate void DelegateMark(int m);
+    class Teacher
+    {
+        public String Name { get; set; }
+        public DateTime Bday { get; set; }
+        public override string ToString() => $"|{Name,-10}|{Bday.ToShortDateString(),12}|";
+
+        public event DelegateMark Exam;
+
+        DelegateMark[] hal = new DelegateMark[3];
+        public event DelegateMark ExamThree12
+        {
+            add
+            {
+                for (int i = 0; i < hal.Length; i++)
+                {
+                    if (hal[i] is null)
+                    {
+                        hal[i] = value;
+                        break;
+                    }
+                }
+            }
+            remove
+            {
+
+                for (int i = 0; i < hal.Length; i++)
+                {
+                    if (hal[i] == value)
+                    {
+                        hal[i] = null;
+                        break;
+                    }
+                }
+
+
+            }
+        }
+
+        public void SetMark()
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            //Exam(rnd.Next(1,13));
+            //if (Exam!=null)
+            //    Exam.Invoke(rnd.Next(1,13));
+            Exam?.Invoke(rnd.Next(1, 13));
+        }
+        public void SetMarkH()
+        {
+            foreach (var method in hal)
+            {
+                method?.Invoke(55);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Test()
+        {
+            Teacher teacher = new Teacher
+            {
+                Name = "Ivan",
+                Bday = new DateTime(1996, 10, 25),
+
+            };
+
+
+            Student st1 = new Student
+            {
+                Name = "Piter",
+                Bday = new DateTime(1996, 10, 25),
+                marks = new List<int> { 10 }
+            };
+            Console.WriteLine(st1);
+            Aspirant asp1 = new Aspirant
+            {
+                Name = "Stepan",
+                Bday = new DateTime(2013, 10, 25),
+                marks = new List<int> { 3 }
+            };
+            Console.WriteLine(asp1);
+
+            teacher.Exam += Start_Exam;
+            teacher.Exam += st1.SetMark;
+            teacher.Exam += asp1.SetOcinka;
+            teacher.Exam += Finish_Exam;
+            teacher.SetMark();
+            Console.WriteLine(st1);
+            Console.WriteLine(asp1);
+        }
+        static void Test2()
+        {
+            Teacher teacher = new Teacher
+            {
+                Name = "Ivan",
+                Bday = new DateTime(1996, 10, 25),
+            };
+            Student[] gr = {
+                new Student { Name = "Piter", Bday = new DateTime(1996, 10, 25),
+                                marks = new List<int> { 10,5,6 }},
+                 new Student { Name = "Ira", Bday = new DateTime(1996, 10, 25),
+                                marks = new List<int> { 10,6 }},
+                  new Student { Name = "Anna", Bday = new DateTime(1996, 10, 25),
+                                marks = new List<int> { 5,6 }},
+                   new Student { Name = "Igor", Bday = new DateTime(1996, 10, 25),
+                                marks = new List<int> { 8}}
+            };
+            Aspirant[] gra = {
+                new Aspirant { Name = "APiter", Bday = new DateTime(1996, 10, 25),
+                                marks = new List<int> { 11,10,5,6 }},
+                 new Aspirant { Name = "AIra", Bday = new DateTime(1996, 10, 25),
+                                marks = new List<int> { 10,6 ,4}},
+                  new Aspirant { Name = "AAnna", Bday = new DateTime(1996, 10, 25),
+                                marks = new List<int> { 5,8,6 }},
+                   new Aspirant { Name = "AIgor", Bday = new DateTime(1996, 10, 25),
+                                marks = new List<int> { 8,9}}
+            };
+
+            Console.WriteLine($"{String.Join("\n", gr.ToList())}");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"{String.Join("\n", gra.ToList())}");
+
+            for (int i = 0; i < gr.Length; i++)
+                teacher.Exam += gr[i].SetMark;
+
+            for (int i = 0; i < gra.Length; i++)
+                teacher.Exam += gra[i].SetOcinka;
+
+            teacher.Exam -= gr[1].SetMark;
+
+            for (int i = 0; i < gr.Length; i++)
+                teacher.ExamThree12 += gr[i].SetMark;
+
+            teacher.ExamThree12 -= gr[2].SetMark;
+
+            for (int i = 0; i < gra.Length; i++)
+                teacher.ExamThree12 += gra[i].SetOcinka;
+
+            //teacher.Exam += Start_Exam;
+            //teacher.Exam += st1.SetMark;
+            //teacher.Exam += asp1.SetOcinka;
+            //teacher.Exam += Finish_Exam;
+            teacher.SetMark();
+            teacher.SetMarkH();
+
+
+            Console.WriteLine("-------------------MMM--------------------");
+            Console.WriteLine($"{String.Join("\n", gr.ToList())}");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"{String.Join("\n", gra.ToList())}");
+        }
+
+        private static void Start_Exam(int m)
+        {
+            Console.WriteLine("Start_Exam"); ;
+        }
+        private static void Finish_Exam(int m)
+        {
+            Console.WriteLine("Finish_Exam"); ;
+        }
+
+        static void Main(string[] args)
+        {
+            //  Test();
+            Test2();
+        }
+    }
+}
